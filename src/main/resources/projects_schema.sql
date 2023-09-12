ae691b785670940c5c7b23c752468458349a32c7
AndWalling <andrewwalling86@yahoo.com>
2023-09-11 18:50:55 -0700

    week8 update

--- src/main/resources/projects_schema.sql
+++ src/main/resources/projects_schema.sql
@@ -5,31 +5,40 @@ DROP TABLE IF EXISTS material;
 DROP TABLE IF EXISTS project;
 
 CREATE TABLE project (
-project_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+project_id INT AUTO_INCREMENT NOT NULL,
 project_name VARCHAR(128) NOT NULL,
 estimated_hours DECIMAL(7,2),
 actual_hours DECIMAL(7,2),
 difficulty INT,
-notes TEXT
+notes TEXT,
+primary key (project_id)
 );
 CREATE TABLE material (
-material_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-project_id INT NOT NULL FOREIGN KEY REFERENCES project(project_id),
+material_id INT AUTO_INCREMENT NOT NULL,
+project_id INT NOT NULL,
 material_name VARCHAR(128) NOT NULL,
 num_required INT,
-cost DECIMAL(7,2)
+cost DECIMAL(7,2),
+primary key (material_id),
+foreign key (project_id) references project(project_id)
 );
 CREATE TABLE step (
-step_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-project_id INT NOT NULL FOREIGN KEY REFERENCES project(project_id),
+step_id INT AUTO_INCREMENT NOT NULL,
+project_id INT NOT NULL,
 step_text TEXT NOT NULL,
-step_order INT NOT NULL
+step_order INT NOT null,
+primary key (step_id),
+foreign key (project_id) references project(project_id)
 );
 CREATE TABLE category (
-category_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-category_name VARCHAR(128) NOT NULL
+category_id INT AUTO_INCREMENT NOT NULL,
+category_name VARCHAR(128) NOT null,
+primary key (category_id)
 );
 CREATE TABLE project_category (
-project_id INT NOT NULL FOREIGN KEY REFERENCES project(project_id),
-category_id INT NOT NULL FOREIGN KEY REFERENCES category(category_id)
+project_id INT NOT NULL,
+category_id INT NOT null,
+foreign key (project_id) references project(project_id),
+foreign key (category_id) references category(category_id),
+unique key(project_id, category_id)
 );
